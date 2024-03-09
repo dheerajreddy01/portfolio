2e73fe8961c63ec5f84d625f5a7608b14dba46e8
Dheerajreddy <reddydheeraj94@gmail.com>
2024-03-09 11:54:53 -0500

    changes

--- src/Pages/Homescreen/index.jsx
+++ src/Pages/Homescreen/index.jsx
@@ -1,13 +1,22 @@
 
-import HeroSection from "../Home/HeroSection";
+import AboutMe from "../Home/AboutMe";
+import ContactMe from "../Home/ContactMe";
+import Footer from "../Home/Footer";
+import HeroSection from "../HeroSection";
+import MyPortfolio from "../Home/MyPortfolio";
+import MySkills from "../Home/MySkills";
+import Testimonial from "../Home/Testimonials";
 
-
-export default function Home(){
-
-    return(
-        <>
-        <HeroSection/>
-        
-        </>
-    );
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <MySkills/>
+      <AboutMe />
+      <MyPortfolio />
+      <Testimonial />
+      <ContactMe />
+      <Footer />
+    </>
+  );
 }
